4f8325ccaba3ab85ce5ece70fefda561cd76641a
italo <italosp00@gmail.com>
2021-05-08 23:03:41 -0300

    Now I'm using the geckodriver to use firefox instead of chrome. And I added a way to call the script with an argument throgh sys.argv(). Thanks to the python package Loguru, now we have a pretty and out of the box way to have logs within the code.

--- app.py
+++ app.py
@@ -1,26 +1,35 @@
-from selenium import webdriver
-from decouple import config
-import pyautogui
 import time
+from os import getenv
+from sys import argv, exit
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from dotenv import load_dotenv
+import pyautogui
+from loguru import logger
 
+@logger.catch
 class SigaaBot:
+
     def __init__(self):
         #credenciais
-        self.user = config('USUARIO')
-        self.passw = config('PASS')
-        self.eixoX = 512
-        self.eixoY_ensino = 179
-        options = webdriver.ChromeOptions()
-        options.add_argument('lang=pt-br')
-        options.add_argument('start-maximized')
-        #as duas linhas abaixo removem a msgm de chrome sendo testado por software de automacao
-        options.add_experimental_option("useAutomationExtension", False)
-        options.add_experimental_option("excludeSwitches",["enable-automation"])
-        self.driver = webdriver.Chrome(executable_path=r'./chromedriver', options=options)
+        load_dotenv()
+        try:
+            logger.debug(argv[1])
+            self.command = argv[1]
+        except IndexError as e:
+            logger.debug(e)
+            logger.debug("Passe um ano letivo da UFPA.")
+            exit()
+        self.user = getenv('SIGAA_USER')
+        self.passw = getenv('SIGAA_PASS')
+        #option = Options()
+        #option.headless = True
+        # executable_path is to the webdriver locate the binary of the driver
+        self.driver = webdriver.Firefox(executable_path='/home/italo/Documents/python_projects/sigaa_bot/geckodriver')
 
-    def abrir_sigaa(self):
+    def sigaa(self):
         self.driver.get('https://sigaa.ufpa.br/sigaa/verTelaLogin.do;jsessionid=6EC32AADFAC73E4050E1CCDC6BAB805C.bacaba2')
-        time.sleep(2)
+        time.sleep(5)
         campo_user = self.driver.find_element_by_name("user.login")
         campo_user.click()
         campo_user.send_keys(self.user)
@@ -29,13 +38,12 @@ class SigaaBot:
         campo_passw.send_keys(self.passw)
         botao_entrar = self.driver.find_element_by_xpath("//input[@type='submit']")
         botao_entrar.click()
-    
-    def selecionar_periodo_discente(self):
-        periodo = self.driver.find_element_by_partial_link_text("2021-1")
+        periodo = self.driver.find_element_by_partial_link_text(self.command)
         periodo.click()
-        time.sleep(2)
+        time.sleep(5)
         discente = self.driver.find_element_by_partial_link_text("Discente")
         discente.click()
+        logger.debug("Success!!")
 
     def ver_notas(self):
         pyautogui.moveTo(self.eixoX, self.eixoY_ensino) #menu ensino
@@ -45,13 +53,13 @@ class SigaaBot:
         pyautogui.scroll(-20, x=512, y=900)
         time.sleep(8)
 
-    def emitir_atestado(self):
+    def ver_atestado(self):
         pyautogui.moveTo(self.eixoX, self.eixoY_ensino) #menu ensino
         pyautogui.moveTo(self.eixoX, 244)
         pyautogui.click()
         time.sleep(10)
 
-    def emitir_historico(self):
+    def ver_historico(self):
         pyautogui.moveTo(self.eixoX, self.eixoY_ensino) #menu ensino
         pyautogui.moveTo(self.eixoX, 251)
         pyautogui.click()
@@ -65,33 +73,8 @@ class SigaaBot:
         time.sleep(30)
         #não abre janela, logo não precisa do método voltar
 
-    #fazer essa porra funcionar
-    def imprimir(self):
-        imprime = self.driver.find_element_by_partial_link_text("Imprimir")
-        imprime.click()
-        time.sleep(15)
-
-    def voltar(self):
-        voltar = self.driver.find_element_by_partial_link_text("Voltar")
-        voltar.click()
-        time.sleep(5)
-
-    def sair(self):
-        sair = self.driver.find_element_by_partial_link_text("SAIR")
-        sair.click()
-        time.sleep(5)
-        pyautogui.hotkey('alt', 'f4')
 
 robot = SigaaBot()
-robot.abrir_sigaa()
-robot.selecionar_periodo_discente()
-robot.ver_notas()
-robot.voltar()
-robot.emitir_atestado()
-#robot.voltar()
-#robot.emitir_historico()
-#robot.emitir_declaracao_vinculo()
-#robot.sair()
-
+robot.sigaa()
 #falta resolver o problema do fechamento automatico do chrome, nos breakpoints acima
 #e como apertar os botões imprimir acima
